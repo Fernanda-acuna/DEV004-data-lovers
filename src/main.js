e7e4d9994a657523def6bfa8b7de8513b70e7636
Fernanda Acuna <fdafilgueira@gmail.com>
2023-02-16 18:07:30 -0300

    prueba

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from  './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// console.log(data.films);
+
+const peliculas = data.films;
+let tituloPeliculas = document.getElementById("title")
+for(let i = 0; i < peliculas.length; i++){
+    tituloPeliculas.innerText = peliculas[i].poster
+    console.log(tituloPeliculas)
+    //"<div>{pelicula[i].title}</div>"
+}
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,6 @@
 import { example, anotherExample } from '../src/data.js';
 
-
+//llenar estos ejemplos para rellenar con nuestras funciones.
 describe('example', () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
