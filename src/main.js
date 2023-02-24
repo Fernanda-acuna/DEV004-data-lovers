77eddf6a9253fa11f727c6c61171bed21a94d19b
debrasbread <figueroa.debra@gmail.com>
2023-02-24 14:51:42 -0600

    Muestra títulos aún sin estilos

--- src/main.js
+++ src/main.js
@@ -16,4 +16,29 @@ for (let i = 0; i < peliculas.length; i++) {
 }
 
 
+const tituloPelicula = data.films
+const contenedorNombrePelicula = document.querySelector("#container-title")
+console.log(tituloPelicula)
+
+for (let i = 0; i < tituloPelicula.length; i++) {
+  const nombreDePeli = tituloPelicula[i].title;
+  console.log(nombreDePeli)
+
+  const divTitulo = document.createElement("div");
+
+  divTitulo.classList.add("flex-item");
+
+  const textoPelis = document.createElement("p");
+  console.log(textoPelis)
+
+  textoPelis.textContent = peliculas[i].title;
+  divTitulo.append(textoPelis);
+
+  contenedorNombrePelicula.append(divTitulo);
+  console.log(divTitulo)
+}
+
+
+
+
 
