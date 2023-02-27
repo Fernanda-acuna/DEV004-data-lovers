b5d18cb77868128bd73afc77357262031c695c9b
debrasbread <figueroa.debra@gmail.com>
2023-02-27 14:32:24 -0600

    Muestra títulos y portadas en cuadrícula

--- src/main.js
+++ src/main.js
@@ -5,38 +5,27 @@ import data from './data/ghibli/ghibli.js';
 const peliculas = data.films;
 const contenedorPeliculas = document.querySelector("#container-movies");
 
-for (let i = 0; i < peliculas.length; i++) {
-  const posterPelicula = peliculas[i].poster;
-  const divPelis = document.createElement("div");
-  divPelis.classList.add("flex-item");
-  const imgPelis = document.createElement("img");
-  imgPelis.src = posterPelicula;
-  divPelis.append(imgPelis);
-  contenedorPeliculas.append(divPelis);
-}
-
-
-const tituloPelicula = data.films
-const contenedorNombrePelicula = document.querySelector("#container-title")
-console.log(tituloPelicula)
+const tituloPelicula = data.films;
+const contenedorNombrePelicula = document.querySelector("#container-title");
+// const directorPelicula = data.director;
 
 for (let i = 0; i < tituloPelicula.length; i++) {
   const nombreDePeli = tituloPelicula[i].title;
-  console.log(nombreDePeli)
+  const posterPelicula = peliculas[i].poster;
+//  const nombreDirector= directorPelicula[i].director;
+//  console.log(nombreDirector)
+  const html= `<section class="grid-item"><p>${nombreDePeli}</p> <img src="${posterPelicula}" /></section>`;
+//const html = "<p>"+nombreDePeli+"</p><img src=""+postPelicula+"/>";
+console.log(html); 
+// debugger
+contenedorNombrePelicula.innerHTML= contenedorNombrePelicula.innerHTML + html;
+// const directorHtml= `<section class="grid-item"><p>${nombreDirector}</p></section>`;
 
-  const divTitulo = document.createElement("div");
-
-  divTitulo.classList.add("flex-item");
-
-  const textoPelis = document.createElement("p");
-  console.log(textoPelis)
-
-  textoPelis.textContent = peliculas[i].title;
-  divTitulo.append(textoPelis);
-
-  contenedorNombrePelicula.append(divTitulo);
-  console.log(divTitulo)
 }
+
+
+
+
 
 
 
