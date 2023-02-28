30f77dc219c49d595f266763c82141bc8497df8b
debrasbread <figueroa.debra@gmail.com>
2023-02-28 14:57:16 -0600

    Muestra títulos en azul

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 const peliculas = data.films;
-const contenedorPeliculas = document.querySelector("#container-movies");
+// const contenedorPeliculas = document.querySelector("#container-movies");
 
 const tituloPelicula = data.films;
 const contenedorNombrePelicula = document.querySelector("#container-title");
@@ -12,14 +12,15 @@ const contenedorNombrePelicula = document.querySelector("#container-title");
 for (let i = 0; i < tituloPelicula.length; i++) {
   const nombreDePeli = tituloPelicula[i].title;
   const posterPelicula = peliculas[i].poster;
-//  const nombreDirector= directorPelicula[i].director;
-//  console.log(nombreDirector)
-  const html= `<section class="grid-item"><p class="letraRoja">${nombreDePeli}</p> <img src="${posterPelicula}" /></section>`;
-//const html = "<p>"+nombreDePeli+"</p><img src=""+postPelicula+"/>";
+    //  const nombreDirector= directorPelicula[i].director;
+    //  console.log(nombreDirector)
+  const html=`<section class="grid-item"><p class="letraRoja">${nombreDePeli}</p> <img src="${posterPelicula}" class="cortinita"/></section>`;
+    // const VerPelis= `<section <h1 class="main-title">Producciones Ghibli</h1> </section>`;
+    //const html = "<p>"+nombreDePeli+"</p><img src=""+postPelicula+"/>";
 console.log(html); 
-// debugger
+    // debugger
 contenedorNombrePelicula.innerHTML= contenedorNombrePelicula.innerHTML + html;
-// const directorHtml= `<section class="grid-item"><p>${nombreDirector}</p></section>`;
+    // const directorHtml= `<section class="grid-item"><p>${nombreDirector}</p></section>`;
 
 }
 
