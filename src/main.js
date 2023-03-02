6c986fc9b2c9f29a4035a29e5697585ab61ca19a
debrasbread <figueroa.debra@gmail.com>
2023-03-02 13:41:38 -0600

    Muestra directores y años en el navegador abajo de los pósters

--- src/main.js
+++ src/main.js
@@ -1,56 +1,36 @@
 import { example } from './data.js';
-
-
 import data from './data/ghibli/ghibli.js';
 
 const peliculas = data.films;
-// const contenedorPeliculas = document.querySelector("#container-movies");
 
-const tituloPelicula = data.films;
-const contenedorNombrePelicula = document.querySelector("#container-title");
-// const directorPelicula = data.director;
+const contenedorPeliculas = document.querySelector("#contenedor-peliculas");
+console.log(contenedorPeliculas);
 
-for (let i = 0; i < tituloPelicula.length; i++) {
-  const nombreDePeli = tituloPelicula[i].title;
-  const posterPelicula = peliculas[i].poster;
-    //  const nombreDirector= directorPelicula[i].director;
-    //  console.log(nombreDirector)
-  const html=`<section class="grid-item"><p class="letraRoja">${nombreDePeli}</p> <img src="${posterPelicula}" class="cortinita"/></section>`;
-    // const VerPelis= `<section <h1 class="main-title">Producciones Ghibli</h1> </section>`;
-    //const html = "<p>"+nombreDePeli+"</p><img src=""+postPelicula+"/>";
-console.log(html); 
-    // debugger
-contenedorNombrePelicula.innerHTML= contenedorNombrePelicula.innerHTML + html;
-    // const directorHtml= `<section class="grid-item"><p>${nombreDirector}</p></section>`;
-
-}
-
-
-
-
-
-
-
-
-/*
-import data from './data/ghibli/ghibli.js';
-
-const peliculas = data.films;
-const contenedorNombrePelicula = document.querySelector("#container-title");
 
 for (let i = 0; i < peliculas.length; i++) {
-  const { title, poster } = peliculas[i];
-  const html = `
-    <section class="grid-item">
-      <p class="letraRoja">${title}</p>
-      <img src="${poster}" class="cortinita"/>
-    </section>
-  `;
-  contenedorNombrePelicula.innerHTML += html;
-}
-*/
+  const nombreDePeli = peliculas[i].title;
+  const posterPelicula = peliculas[i].poster;
+  const nombreDirector = peliculas[i].director;
+  const yearMovie = peliculas[i].release_date;
 
-// EVENTOS PRUEBA
+  const html=`<section class="grid-item">
+  <p class="titulosPelis">${nombreDePeli}</p> 
+  <img src="${posterPelicula}" class="cortinita"/>
+  <div class="overlay">
+  <div class="directorPeli"> 
+  <p class="directores"> ${nombreDirector}</p>
+  </div>
+  <div class="fechaLanzamiento">
+  <p class="anio"> ${yearMovie}</p>
+  </div>
+  </div>
+  </section>`;
+   
+  // debugger
+  contenedorPeliculas.innerHTML= contenedorPeliculas.innerHTML + html;
+   
+
+}
 
 
 const gridItems = document.querySelectorAll('.grid-item');
@@ -58,13 +38,9 @@ const gridItems = document.querySelectorAll('.grid-item');
 gridItems.forEach(item => {
   const img = item.querySelector('img');
   item.addEventListener('mouseover', () => {
-    img.classList.add('img-overlay');
+    img.classList.add(`img-overlay`);
   });
   item.addEventListener('mouseout', () => {
-    img.classList.remove('img-overlay');
+    img.classList.remove(`img-overlay`);
   });
 });
-
-
-
-
