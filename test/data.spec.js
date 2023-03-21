5401c02d7b21376ca913617e5eb0a9a26717812d
Fernanda Acuña <fdafilgueira@gmail.com>
2023-03-21 09:45:51 -0300

    agrega el primer intento de test para orden ascendente

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,34 @@
-import { example, anotherExample } from '../src/data.js';
+import  peliculasAscendente  from '../src/data.js';
+
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
+
+//   it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('Ordenar pelîculas de manera Ascendente', () => {
+  it('funcion que ordena de la mas antigua a la mas nueva', () => {
+    const datos = [
+      {release_date: "1986"},
+      {release_date: "1988"},
+      {release_date: "1991"},
+      {release_date: "1992"}
+    ]
+
+    const ordenarDeAntiguaANueva = peliculasAscendente(datos)
+    expect(typeof ordenarDeAntiguaANueva[0]).release_date.toBe("1986");
+    expect(typeof ordenarDeAntiguaANueva[1]).release_date.toBe("1988");
+    expect(typeof ordenarDeAntiguaANueva[2]).release_date.toBe("1991");
+    expect(typeof ordenarDeAntiguaANueva[3]).release_date.toBe("1992");
+    
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
 });
